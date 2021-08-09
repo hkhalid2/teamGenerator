2102863ddc7120e8d3ca153202a5907b05243d0b
Hamza Khalid <hamzer95@gmail.com>
2021-08-09 16:02:07 -0400

    created init function to run prompts, created team array to hold team members to generate html file from and finally got overall app prompts to function untilising prompts and given acceptance criteria

--- develop/index.js
+++ develop/index.js
@@ -2,9 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+let team = [];
 
 const ManagerQuestions = () => {
     return inquirer.prompt([
@@ -31,8 +32,13 @@ const ManagerQuestions = () => {
             type: "input",
             name: "officeNumber",
             message: "What is the Manager's officeNumber?",
-          },
+        },
     ])
+
+        .then((answers) => {
+            let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            team.push(manager);
+        })
 };
 
 const EngineerQuestions = () => {
@@ -60,8 +66,13 @@ const EngineerQuestions = () => {
             type: "input",
             name: "github",
             message: "What is the Engineer's github?",
-          },
+        },
     ])
+        .then((answers) => {
+            let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            team.push(engineer);
+            createTeam();
+        })
 };
 
 const InternQuestions = () => {
@@ -89,8 +100,13 @@ const InternQuestions = () => {
             type: "input",
             name: "school",
             message: "What school is the Intern attending?",
-          },
+        },
     ])
+        .then((answers) => {
+            let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            team.push(intern);
+            createTeam();
+        })
 };
 
 const createTeam = () => {
@@ -100,11 +116,39 @@ const createTeam = () => {
             name: 'menu',
             message: 'What team member would you like to add?',
             choices: [
-             'Intern',
-             'Engineer',
-             'Finish building team'
+                'Intern',
+                'Engineer',
+                'Finish building team'
             ],
             initial: 1
-          },
+        },
     ])
-}
+        .then((answer) => {
+            if (answer.menu == 'Intern') {
+                InternQuestions();
+            }
+            else if (answer.menu == 'Engineer') {
+                EngineerQuestions();
+            }
+            else {
+                console.log('Team Finished, HTML file generated');
+            }
+
+
+        });
+
+};
+
+// uses promises to create writeFile function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+const init = () => {
+    console.log("Welcome to the Team Generator app!");
+    ManagerQuestions()
+        .then(() => createTeam())
+        .catch((err) => console.error(err));
+    
+};
+
+init();
